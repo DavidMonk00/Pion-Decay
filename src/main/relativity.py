ca8436f09e5ab146e760104235973eb188eb23cf
DavidMonk00 <DavidMonk00@users.noreply.github.com>
2015-11-24 21:26:03 +0000

    Update relativity.py
    Tidied up a bit

--- src/main/relativity.py
+++ src/main/relativity.py
@@ -14,33 +14,26 @@ def inner(v1, v2):
     return v1.temporal()*v2.temporal() - dot(v1.spatial(), v2.spatial())
  
 class FourVector:
-    """Four-vector class for use in relativity"""
+    '''Four-vector class for use in relativity'''
     def __init__(self, temporal = 0, spatial=np.zeros(3).reshape(3,1)):
         self.spatial = np.array(spatial).reshape(3,1)
         self.temporal = temporal
-     
     def __str__(self):
-        return "(%g, %g, %g, %g)"%(self.temporal,tuple(self.spatial)[0],tuple(self.spatial)[1],tuple(self.spatial)[2])                                    
-                                                                                                                 
+        return "(%g, %g, %g, %g)"%(self.temporal,tuple(self.spatial)[0],tuple(self.spatial)[1],tuple(self.spatial)[2])                                                                                                                                               
     def __add__(self, other):
         return FourVector(self.temporal + other.temporal, self.spatial + other.spatial)
-     
     def __iadd__(self, other):
         self.temporal += other.temporal
         self.spatial += other.spatial
         return self
-     
     def __sub__(self, other):
         return FourVector(self.temporal - other.temporal, self.spatial - other.spatial)
-     
     def __isub__(self, other):
         self.temporal -= other.temporal
         self.spatial -= other.spatial
         return self
-     
     def magsquare(self):
-        return inner(self,self)
-         
+        return inner(self,self)      
     def boost(self, beta):
         x,y,z = 0,1,2
         gamma = 1/np.sqrt(1-dot(beta,beta))
@@ -56,10 +49,6 @@ class PositionFourVector(FourVector):
     def __repr__(self):
         return "%s(ct = %g, r = (%g, %g, %g))"%("PositionFourVector",self.temporal,tuple(self.spatial)[0],tuple(self.spatial)[1],tuple(self.spatial)[2])
      
- 
 class EnergyFourVector(FourVector):
     def __repr__(self):
         return "%s(E/c = %g, p = (%g, %g, %g))"%("EnergyFourVector",self.temporal,tuple(self.spatial)[0],tuple(self.spatial)[1],tuple(self.spatial)[2])
-
-#class VelocityFourVector(FourVector):
-#    def __init__(self, initial velcocity):
