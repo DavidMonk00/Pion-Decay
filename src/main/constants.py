199ed161b724d9943efb705bdb34fc8c1fa2461a
DavidMonk00 <d.g.monk0@gmail.com>
2015-11-24 15:06:52 +0000

    Create constants.py
    Classes containing physical constants used elsewhere

---
+++ src/main/constants.py
@@ -0,0 +1,20 @@
+'''
+Created on 17 Nov 2015
+
+@author: david
+'''
+
+from numpy import array
+
+class Constants:
+    '''
+    A class of fundamental physical constants
+    '''
+    c = 3e8
+    e = 1.6e-19
+    dimensions = array([[5],[100]])
+    
+class Mass:
+    electron = 0.5
+    pion = 139.6
+    muon = 105.7
