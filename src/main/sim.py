f450a40a490b4345e99fdc41fe8c7f5a5329d25c
DavidMonk00 <d.g.monk0@gmail.com>
2015-11-24 16:14:58 +0000

    Update sim.py
    Added plot function into main sim script

--- src/main/sim.py
+++ src/main/sim.py
@@ -4,102 +4,64 @@ Created on 17 Nov 2015
 @author: david
 '''
 
+import particle as p
 import numpy as np
-import relativity as rel
-import constants as constants
-from numpy import Inf
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+from __builtin__ import int
 
-const = constants.Constants()
-Mass = constants.Mass()
-
-def Michel():
-    x, y = np.random.random(2)
-    if x >= y:
-        return 0.25 + x * 52.75
-    else:
-        return 0.25 + y * 52.75
-
-class Particle:
-    exit = False
-    def __init__(self, energyvector, initial_position, mass = 1, lifetime = 0):
-        ''' Initial position is a PostionFourVector class, with t = 0
-            energy is a float which is the energy of the particle in MeV'''
-        self.energyvector = energyvector
-        self.pos = initial_position
-        self.mass = mass
-        self.gamma = self.energyvector.temporal/mass
-        self.beta = energyvector.spatial/(self.gamma*mass)
-        self.decay_time = np.random.exponential(self.gamma*lifetime)
-    
-          
-class Pion(Particle):
-    branching = 1e-4
-    def __init__(self, energy, initial_position = rel.PositionFourVector()):
-        mass = Mass.pion #in MeV
-        lifetime = 2.6e-8
-        energyvector = rel.EnergyFourVector(energy,(0,0,np.sqrt(energy*energy - mass*mass)))
-        Particle.__init__(self, energyvector, initial_position, mass, lifetime)
-    def Decay(self):
-        ct = const.c*self.decay_time
-        self.decay_pos = rel.PositionFourVector(self.pos.temporal + ct, ct*self.beta)
-        if self.decay_pos.spatial[2] > 100:
-            self.type = "e"
-            self.exit = True
-            return None
-        _phi = 2*np.pi*np.random.uniform()
-        _costheta = 2*np.random.uniform() - 1
-        _theta = np.arccos(_costheta)
-        if np.random.uniform() < self.branching:
-            self.type = "e"
-            energy = (self.mass*self.mass + Mass.electron*Mass.electron)/(2*self.mass) #Neutrino mass is neglected
-            p = np.sqrt(energy*energy - Mass.electron*Mass.electron)
-            return Electron(rel.EnergyFourVector(energy,(p*np.sin(_theta)*np.cos(_phi),
-                                                         p*np.sin(_theta)*np.sin(_phi),
-                                                         p*_costheta)).boost(-self.beta),
-                            self.decay_pos)
+def ParticleSim(energy):
+    '''Returns position of final particle's exit from detector'''
+    pi = p.Pion(energy)
+    if pi.DecayCheck() == False:
+        decay_part = pi.Decay()
+        if pi.type == 'e':
+            return 'electron', decay_part.ExitPosition()
         else:
-            self.type = "mu"
-            energy = (self.mass*self.mass + Mass.muon*Mass.muon)/(2*self.mass) #Neutrino mass is neglected
-            p = np.sqrt(energy*energy - Mass.muon*Mass.muon)
-            return Muon(rel.EnergyFourVector(energy,(p*np.sin(_theta)*np.cos(_phi),
-                                                         p*np.sin(_theta)*np.sin(_phi),
-                                                         p*_costheta)).boost(-self.beta),
-                        self.decay_pos) 
+            mu = decay_part
+            if mu.DecayCheck() == False:
+                el = mu.Decay()
+                return 'muon-electron', el.ExitPosition()
+            else:
+                return 'muon', mu.ExitPosition()
+    else:
+        return 'pion', pi.exit_pos
+         
+def PlotExit3D(n,energy):
+    pi = [[],[],[]]
+    mu = [[],[],[]]
+    e = [[],[],[]]
+    em = [[],[],[]]
+    for i in xrange(n):
+        p_type, p_pos = ParticleSim(energy)
+        if p_type == 'electron':
+            e[0].append(p_pos[0])
+            e[1].append(p_pos[1])
+            e[2].append(p_pos[2])
+        elif p_type == 'pion':
+            pi[0].append(p_pos[0])
+            pi[1].append(p_pos[1])
+            pi[2].append(p_pos[2])
+        elif p_type == 'muon':
+            mu[0].append(p_pos[0])
+            mu[1].append(p_pos[1])
+            mu[2].append(p_pos[2])
+        elif p_type == 'muon-electron':
+            em[0].append(p_pos[0])
+            em[1].append(p_pos[1])
+            em[2].append(p_pos[2])  
+    fig = plt.figure()
+    ax = fig.add_subplot(111, aspect='equal', projection='3d')
+    ax.scatter(pi[0], pi[1], pi[2], c = 'k', s = 1)
+    ax.scatter(mu[0], mu[1], mu[2], c = 'k', s = 1)
+    ax.scatter(e[0], e[1], e[2], c = 'r', s = 25)
+    ax.scatter(em[0], em[1], em[2], c = 'b', s = 25)
+    plt.show()
 
-class Electron(Particle):
-    def __init__(self, energyvector, initial_position):
-        mass = Mass.electron #in MeV
-        lifetime = np.Inf
-        Particle.__init__(self, energyvector, initial_position, mass, lifetime)
-    def ExitPosition(self):
-        ct = np.linspace(0,(const.dimensions[1] - self.pos.spatial[2])/self.beta[2],1e5)
-        pos = self.pos.spatial +  ct*self.beta
-        pos_cyl = np.array([[np.sqrt((pos[0]*pos[0]) + (pos[1]*pos[1]))],[pos[2]]]).reshape(2,pos.shape[1])
-        exit_bool  = pos_cyl < const.dimensions.reshape(2,1)
-        for i in xrange(int(pos.shape[1])):
-            if exit_bool[:,i].all() != True:
-                return pos[:,i]
-                break
-        
 
-class Muon(Particle):
-    def __init__(self, energyvector, initial_position):
-        mass = Mass.muon
-        lifetime = 2.2e-6
-        Particle.__init__(self, energyvector, initial_position, mass, lifetime)
-    def Decay(self):
-        ct = const.c*self.decay_time
-        self.decay_pos = rel.PositionFourVector(self.pos.temporal + ct, ct*self.beta)
-        if self.decay_pos.spatial[2] > 100:
-            self.exit = True
-            return None
-        _phi = 2*np.pi*np.random.uniform()
-        _costheta = 2*np.random.uniform() - 1
-        _theta = np.arccos(_costheta)
-        energy = Michel()
-        p = np.sqrt(energy*energy - Mass.electron*Mass.electron)
-        return Electron(rel.EnergyFourVector(energy,(p*np.sin(_theta)*np.cos(_phi),
-                                                         p*np.sin(_theta)*np.sin(_phi),
-                                                         p*_costheta)).boost(-self.beta),
-                            self.decay_pos)
-       
+def main():
+    PlotExit3D(int(1e5), 500)
+    
+    
+if __name__ == '__main__':
+    main()
