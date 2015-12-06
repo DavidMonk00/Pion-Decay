c15461f61c6a46688f401e1bfed2446fe39ca45b
DavidMonk00 <DavidMonk00@users.noreply.github.com>
2015-12-06 21:14:05 +0000

    Update datagen.py
    Added divisor for number of iterations

--- src/main/datagen.py
+++ src/main/datagen.py
@@ -19,17 +19,16 @@ class Gen:
     def EnergyDepositDumb(self, n, detector):
         sim = Simulation(detector.position)   
         f = open('010190/%s.data'%self.energy,'w')
-        fb = open('010190/%s.databackup'%self.energy,'a')
+        #fb = open('010190/%s.databackup'%self.energy,'a')
         for i in xrange(long(n)):
             e = sim.ParticleDetect(self.energy)
             if e != 0:
+                #print e
                 e_str = "%.2f"%e
-            #if len(e_str) < 6:
-            #    e_str = '0' + e_str          
                 f.write(e_str+'\n')
-                fb.write(e_str+'\n')
+                #fb.write(e_str+'\n')
         f.close()
-        fb.close()
+        #fb.close()
     def UploadData(self):
         ftp = FTPExt()
         busy = True
@@ -45,10 +44,13 @@ class Gen:
 def main():
     detector = Detector(np.array([[0.1],[0.1],[90]]))
     x = Gen(10000)
-    for i in range(1000):
-        print "%s%%"%(float(i)/float(1000))
+    cpus = 4
+    iterations =  1e7
+    loops = iterations/(cpus*10000)
+    for i in xrange(int(loops)):
+        print "%s%%"%(float(i)/loops*100)
         x.EnergyDepositDumb(10000, detector)
-        x.UploadData()
+        #x.UploadData()
 
 if __name__ == "__main__":
     main()
